82e466a2e192f88951b1fb55c060e6c772e470e8
lownacereno <lcdacereno@gmail.com>
2022-07-09 16:56:03 -0400

    paso-de-datos-entre-pantallas

---
+++ SkyNoff App/Controller/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  SkyNoff App
+//
+//  Created by Lowna Letticia Cereño Franceschi on 07-07-22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
